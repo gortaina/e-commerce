491d37d80c178940e177914c4cb49c20960027a8
gortaina <joshua.kenzo@gmail.com>
2022-02-08 03:26:55 -0300

    [bootstrap] páginas de navegação adicionadas
    [bootstrap] páginas de navegação adicionadas

--- petstore/settings.py
+++ petstore/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'petstore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        #'DIRS': [],
+        #Após a criação do app pages
+        "DIRS": [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+#após criar o app pages
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field

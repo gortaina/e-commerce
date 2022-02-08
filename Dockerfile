b136fb7949219f42e694407f3d6be96c7e7d2aeb
gortaina <joshua.kenzo@gmail.com>
2022-02-08 01:34:35 -0300

    [docker] configurações  docker

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# syntax=docker/dockerfile:1
+FROM python:3.8
+
+# sem gerar pyc
+ENV PYTHONDONTWRITEBYTECODE=1
+# sem logs em buffer, grava direto
+ENV PYTHONUNBUFFERED=1
+
+WORKDIR /code
+
+RUN python -m pip install --upgrade pip
+COPY requirements.txt .
+RUN pip install -r requirements.txt
+
+COPY . .
+ 
+
--- petstore/settings.py
+++ petstore/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'petstore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',        
+        'USER': 'postgres',        
+        'PASSWORD': 'postgres',        
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -105,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Araguaina'
 
 USE_I18N = True
 
